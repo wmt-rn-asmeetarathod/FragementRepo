e0973289dc79131a4f284b6c8e8f80f7572acafe
wmt-rn-asmeetarathod <asmeetar@webmob.tech>
2021-01-20 16:20:46 +0530

    initial Commit

---
+++ app/src/main/java/com/example/fragementdemo/Fragment2.java
@@ -0,0 +1,26 @@
+package com.example.fragementdemo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment2 extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_2, container, false);
+    }
+}
